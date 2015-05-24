de06f8373ed7291705b077c1585992883e0c6842
chongwehuat <chongweehuat@gmail.com>
2015-05-25 07:55:56 +0800

    update database config

--- config/database.php
+++ config/database.php
@@ -55,9 +55,9 @@ return [
 		'mysql' => [
 			'driver'    => 'mysql',
 			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => env('DB_DATABASE', 'forge'),
+			'database'  => env('DB_DATABASE', 'gogogo2u'),
 			'username'  => env('DB_USERNAME', 'forge'),
-			'password'  => env('DB_PASSWORD', ''),
+			'password'  => env('DB_PASSWORD', 'dni6gielMjtecloDQx1b'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
